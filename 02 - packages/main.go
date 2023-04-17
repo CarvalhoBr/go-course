9774dc28dcdfb3c573e52f4d0dc52b5fa89c4906
Brandon Carvalho <brandonsouza27@gmail.com>
2023-04-17 12:14:44 -0300

    02 - external packages

--- 02 - packages/main.go
+++ 02 - packages/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"packages/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo no arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("brandonnotemail.com")
+	fmt.Println(erro)
 }
